10456ae0d7adca9489d9d4ec80a673a459ccc019
Gabriele Messina <gabryelemessina99@gmail.com>
2022-04-23 22:34:37 +0200

    Initial commit

---
+++ CommunityToolkitMaui380/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CommunityToolkitMaui380;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CommunityToolkitMaui380/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CommunityToolkitMaui380;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
